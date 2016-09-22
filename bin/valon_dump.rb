c57f6a88cf9158835c381afb1eb0e5f133926379
David MacMahon <davidm@astro.berkeley.edu>
2016-09-22 17:09:56 +1000

    Tweak formatting of valon_dump.rb output

--- bin/valon_dump.rb
+++ bin/valon_dump.rb
@@ -17,6 +17,18 @@ label = vs.label
 label = 'Unlabled Valon' if label.empty?
 vco_min, vco_max = vs.vco_range
 
+freq_a_rf  = '%12.6f' % (vs.freq_a_rf  / 1e6)
+freq_a_vco = '%12.6f' % (vs.freq_a_vco / 1e6)
+freq_a_pfd = '%12.6f' % (vs.freq_a_pfd / 1e6)
+a_n  = '%5d' % vs.a_int
+a_n += ' %d/%d' % [vs.a_frac, vs.a_mod] if vs.a_frac != 0
+
+freq_b_rf  = '%12.6f' % (vs.freq_b_rf  / 1e6)
+freq_b_vco = '%12.6f' % (vs.freq_b_vco / 1e6)
+freq_b_pfd = '%12.6f' % (vs.freq_b_pfd / 1e6)
+b_n  = '%5d' % vs.b_int
+b_n += ' %d/%d' % [vs.b_frac, vs.b_mod] if vs.b_frac != 0
+
 puts <<EOF
 Setings for #{label}
 
@@ -24,17 +36,19 @@ Reference: #{vs.is_ext_ref? ? 'External' : 'Internal'}
 Ref Freq : #{vs.ref_freq / 1e6} MHz
 VCO Range: #{vco_min} MHz to #{vco_max} MHz
 
-Synth A: PFD freq #{vs.freq_a_pfd/1e6} MHz
-Synth A: VCO freq #{vs.freq_a_vco/1e6} MHz
-Synth A: RF  freq #{vs.freq_a_rf/1e6} MHz
-Synth A: RF power #{'%+d' % vs.a_dbm} dBm
-Synth A: Locked : #{vs.is_a_locked? ? 'yes' : 'NO'}
+Synth A: OUT freq #{freq_a_rf} MHz
+Synth A: VCO freq #{freq_a_vco} MHz
+Synth A: N divide #{a_n}
+Synth A: PFD freq #{freq_a_pfd} MHz
+Synth A: RF power    #{'%+d' % vs.a_dbm} dBm
+Synth A: Lock #{vs.is_a_locked? ? ' OK' : 'BAD'}
 
-Synth B: PFD freq #{vs.freq_b_pfd/1e6} MHz
-Synth B: VCO freq #{vs.freq_b_vco/1e6} MHz
-Synth B: RF  freq #{vs.freq_b_rf/1e6} MHz
-Synth B: RF power #{'%+d' % vs.b_dbm} dBm
-Synth B: Locked : #{vs.is_b_locked? ? 'yes' : 'NO'}
+Synth B: OUT freq #{freq_b_rf} MHz
+Synth B: VCO freq #{freq_b_vco} MHz
+Synth B: N divide #{b_n}
+Synth B: PFD freq #{freq_b_pfd} MHz
+Synth B: RF power    #{'%+d' % vs.b_dbm} dBm
+Synth B: Lock #{vs.is_b_locked? ? ' OK' : 'BAD'}
 EOF
 
 puts
